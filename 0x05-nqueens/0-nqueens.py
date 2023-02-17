4c4edd7c54e47b67d800c5f0ad657a9866a6d30b
ezra odondi <odondiezra5@gmail.com>
2023-02-17 10:24:31 +0300

    0x05. N Queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,59 +1,44 @@
 #!/usr/bin/python3
-''' Python3 program to solve N Queen '''
+""" N queens """
 import sys
 
 
-if len(sys.argv) != 2:
-    print('Usage: nqueens N')
-    exit(1)
-N = sys.argv[1]
-try:
-    N = int(N)
-
-except:
-    print('N must be a number')
+if len(sys.argv) > 2 or len(sys.argv) < 2:
+    print("Usage: nqueens N")
     exit(1)
 
-if N < 4:
-    print('N must be at least 4')
+if not sys.argv[1].isdigit():
+    print("N must be a number")
     exit(1)
 
-k = 1
+if int(sys.argv[1]) < 4:
+    print("N must be at least 4")
+    exit(1)
+
+n = int(sys.argv[1])
 
 
-def printSolution(board):
-<<<<<<< HEAD
-""" A utility function to print solution """
-=======
-    """ A utility function to print solution """
->>>>>>> edf7d1035eaf240a5f9ea7e096db12608fb50a56
-    queens = []
-    global k
-    k = k + 1
-    for i in range(N):
-        for j in range(N):
-            if board[i][j] == 1:
-                queens.append([i, j])
-    print(queens)
-def isSafe(board, row, col):
-    for i in range(col):
-        if board[row][i]:
-            return False
-    i = row
-    j = col
-    while i >= 0 and j >= 0:
-        if board[i][j]:
-            return False
-        i -= 1
-        j -= 1
-    i = row
-    j = col
-    while j >= 0 and i < N:
-        if board[i][j]:
-            return False
-        i = i + 1
-        j = j - 1
-    return True
-def solveNQUtil(board, col):
-<<<<<<< HEAD
+def queens(n, i=0, a=[], b=[], c=[]):
+    """ find possible positions """
+    if i < n:
+        for j in range(n):
+            if j not in a and i + j not in b and i - j not in c:
+                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
+    else:
+        yield a
 
+
+def solve(n):
+    """ solve """
+    k = []
+    i = 0
+    for solution in queens(n, 0):
+        for s in solution:
+            k.append([i, s])
+            i += 1
+        print(k)
+        k = []
+        i = 0
+
+
+solve(n)
